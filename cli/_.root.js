175fd67a43923bc59e18b0af4efa526eb779934f
ig <ilkergonenc@gmail.com>
2022-01-26 21:16:59 +0300

    fix imports

--- cli/_.root.js
+++ cli/_.root.js
@@ -1,21 +1,11 @@
 'use strict'
 // const root =      'C:/users/ilkergonenc/V5/',
 const root =      './',
-      deploy =    `${root}@deploy`,
-      play =      `${root}@play`,
-      sync =      `${root}@pro`,
-      uiKit =     `${root}@uiKit`,
       cli =       `${root}cli`,
-      raw =       `${root}raw`,
-      resource =  `${root}resource`
+      raw =       `${root}raw`
 
 module.exports = {
   root:           `${root}`,
-
-  deploy:         `${deploy}`,
-  play:           `${play}`,
-  sync:           `${sync}`,
-  uiKit:          `${uiKit}`,
 
   cmd:          `${root}bend.cmd`,
   cli: {
@@ -23,27 +13,14 @@ module.exports = {
     // commander:    `${cli}/bend.js`,
     commands:     `${cli}/cmd/`,
 
-    // bender:       `${cli}/@bender.js`,
-    // flow:         `${cli}/flow/`,
-    // tree:         `${cli}/tree/`,
-    // utils:        `${cli}/util/`,
-
     cache: {
       folder:     `${cli}/_.cache/`,
       file:       `${cli}/_.cache/bend.js`,
       src:        `${cli}/_.cache/bend`
     },
 
-    // gulp:         `${cli}/gulpfile.js`,
-
   },
   
   raw:            `${raw}`,
 
-  resource:       `${resource}`,
-  re : {
-    depends:      `${resource}/libraries`,
-    assets:       `${resource}/assets`,
-  },
-  
 }
